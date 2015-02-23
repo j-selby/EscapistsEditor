54e0be34065092d8014fa85d8a7e7b86d5f3c53e
James <jselby@jselby.net>
2015-02-23 16:55:56 +1100

    Add version headers

--- src/main/java/net/jselby/escapists/EscapistsEditor.java
+++ src/main/java/net/jselby/escapists/EscapistsEditor.java
@@ -20,6 +20,7 @@ import java.util.ArrayList;
  * @author j_selbys
  */
 public class EscapistsEditor {
+    public static final String VERSION = "1.1.1";
     public static final boolean DEBUG = false;
 
     // -- Arguments
@@ -58,7 +59,7 @@ public class EscapistsEditor {
     private static boolean showGUI;
 
     private void start() {
-        System.out.println("The Escapists Editor v0.1");
+        System.out.println("The Escapists Editor v" + VERSION);
         System.out.println("By jselby");
 
         System.out.println("=========================");

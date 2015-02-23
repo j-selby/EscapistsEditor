88f8d7aa236a1b2d3fb8925253fd12d2902596d9
James <1404334+j-selby@users.noreply.github.com>
2015-02-23 19:43:27 +1100

    Increment versioning for release 1.1.3

--- src/main/java/net/jselby/escapists/EscapistsEditor.java
+++ src/main/java/net/jselby/escapists/EscapistsEditor.java
@@ -21,7 +21,7 @@ import java.util.ArrayList;
  * @author j_selbys
  */
 public class EscapistsEditor {
-    public static final String VERSION = "1.1.2";
+    public static final String VERSION = "1.1.3";
     public static final boolean DEBUG = false;
 
     // -- Arguments

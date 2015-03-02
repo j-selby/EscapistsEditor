a85cfea18ef1238c3d2f52ad223848d9c07ef152
James <1404334+j-selby@users.noreply.github.com>
2015-03-02 22:07:34 +1100

    Add better caching to IOUtils

--- src/main/java/net/jselby/escapists/utils/IOUtils.java
+++ src/main/java/net/jselby/escapists/utils/IOUtils.java
@@ -1,19 +1,34 @@
 package net.jselby.escapists.utils;
 
-import java.io.ByteArrayOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
+import java.io.*;
 import java.net.URL;
 
 /**
  * Basic IOUtils replacement.
  */
 public class IOUtils {
+    private final static int BUFFER_SIZE = 8192;
+
     public static byte[] toByteArray(URL url) throws IOException {
         InputStream in = url.openStream();
         ByteArrayOutputStream out = new ByteArrayOutputStream();
 
-        byte[] buf = new byte[1024];
+        byte[] buf = new byte[BUFFER_SIZE];
+        int val;
+        while((val = in.read(buf)) != -1) {
+            out.write(buf, 0, val);
+        }
+
+        in.close();
+
+        return out.toByteArray();
+    }
+
+    public static byte[] toByteArray(File file) throws IOException {
+        InputStream in = new FileInputStream(file);
+        ByteArrayOutputStream out = new ByteArrayOutputStream();
+
+        byte[] buf = new byte[BUFFER_SIZE];
         int val;
         while((val = in.read(buf)) != -1) {
             out.write(buf, 0, val);

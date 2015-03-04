2d5e3b25167275949b93ef66d38985f92c316ef5
James <1404334+j-selby@users.noreply.github.com>
2015-03-04 22:52:28 +1100

    Add blank map for New button

--- src/main/java/net/jselby/escapists/EscapistsEditor.java
+++ src/main/java/net/jselby/escapists/EscapistsEditor.java
@@ -21,7 +21,7 @@ import java.util.ArrayList;
  * @author j_selbys
  */
 public class EscapistsEditor {
-    public static final String VERSION = "1.4.1";
+    public static final String VERSION = "1.4.2";
     public static final boolean DEBUG = false;
 
     // -- Arguments

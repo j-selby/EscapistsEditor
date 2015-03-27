7016ee99076f5ecab24d91045f84858de8237907
James <1404334+j-selby@users.noreply.github.com>
2015-03-28 00:56:00 +1100

    Push to 1.7.10 for release

--- src/main/java/net/jselby/escapists/EscapistsEditor.java
+++ src/main/java/net/jselby/escapists/EscapistsEditor.java
@@ -25,7 +25,7 @@ import java.util.ArrayList;
  * @author j_selbys
  */
 public class EscapistsEditor {
-    public static final String VERSION = "1.4.9";
+    public static final String VERSION = "1.4.10";
     public static final boolean DEBUG = false;
 
     // -- Arguments
@@ -297,6 +297,7 @@ public class EscapistsEditor {
 
     public static void main(String[] args) {
         try {
+            // TODO: Replace JCommander with internal implementation to save disk space
             // Redirect SysOut
             OutputStream fileOut = new FileOutputStream(new File("escapistseditor.log"));
             System.setOut(new LoggingDebugPrintStream(fileOut, System.out));

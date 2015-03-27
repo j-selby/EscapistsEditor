6188b87a00367fc1a6b3f21f6f5cc19b8916a43a
James <1404334+j-selby@users.noreply.github.com>
2015-03-27 23:53:34 +1100

    Seperate implementations from interfaces @ layers + objs

--- src/main/java/net/jselby/escapists/objects/impl/Light.java
+++ src/main/java/net/jselby/escapists/objects/impl/Light.java
@@ -1,4 +1,6 @@
-package net.jselby.escapists.objects;
+package net.jselby.escapists.objects.impl;
+
+import net.jselby.escapists.objects.WorldObject;
 
 import java.awt.*;
 import java.awt.geom.Point2D;

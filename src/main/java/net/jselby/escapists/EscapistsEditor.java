aa7c72f0554aeb2160a34bbb624f9b4bb81a4dd7
James <jselby@jselby.net>
2015-03-31 20:04:11 +1100

    Push to 1.5.2 for release

--- src/main/java/net/jselby/escapists/EscapistsEditor.java
+++ src/main/java/net/jselby/escapists/EscapistsEditor.java
@@ -23,7 +23,7 @@ import java.util.ArrayList;
  * @author j_selbys
  */
 public class EscapistsEditor {
-    public static final String VERSION = "1.5.1";
+    public static final String VERSION = "1.5.2";
     public static final boolean DEBUG = false;
 
     // -- Arguments

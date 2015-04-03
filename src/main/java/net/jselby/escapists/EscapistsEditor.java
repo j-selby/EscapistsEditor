838f5fa1ceae0ac603771899e5e71ee3b54095f8
James <jselby@jselby.net>
2015-04-04 01:14:56 +1100

    Force no-text mode for headless devices

--- src/main/java/net/jselby/escapists/EscapistsEditor.java
+++ src/main/java/net/jselby/escapists/EscapistsEditor.java
@@ -26,6 +26,9 @@ public class EscapistsEditor {
     public static final String VERSION = "1.5.4";
     public static final boolean DEBUG = false;
 
+    // For embedded environments
+    public static final boolean DRAW_TEXT = true;
+
     // -- Arguments
     private String decryptFile;
 
